592c2fee6425205f48e4a9793cab4a06a469776e
amarnath201099 <amarnath201099@gmail.com>
2026-07-05 12:55:09 +0530

    Made changes in settings to accept the allowed origin of vercel

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-recipe-book-demo-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # false in production
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["recipe-book-django.vercel.app",".vercel.app",]
 
 
 # Application definition
