89c94f5599d353f6cb18c8379d48c3ceafb5f9f7
amarnath201099 <amarnath201099@gmail.com>
2026-07-05 13:47:41 +0530

    editied footer

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-recipe-book-demo-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # false in production
 
-ALLOWED_HOSTS = ["recipe-book-django.vercel.app",".vercel.app",]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","recipe-book-django.vercel.app",".vercel.app",]
 
 
 # Application definition
